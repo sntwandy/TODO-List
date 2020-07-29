9a853ffad707a40d9ff11cc2ab277e49a8de1307
sntwandy <sntwandy@gmail.com>
2020-07-29 01:01:35 -0400

    Adding ID from Redux State Global

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Card = ({ title, description }) => {
+const Card = ({ id, title, description }) => {
   return(
     <div>
+      <span>ID: {id}</span>
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
